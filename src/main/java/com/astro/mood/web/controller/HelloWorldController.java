4315aaa5100af05426c71a06846ad5d17d0f0cf0
opllip <opllip@naver.com>
2025-01-06 20:04:05 +0900

    gitignore 수정

---
+++ src/main/java/com/astro/mood/web/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.astro.mood.web.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Tip : @RestController = @Controller + @ResponseBody
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
