0eb3e6882ad88e8dcc9cd5f5a33248b0917b7c63
zzoeun <pje308@naver.com>
2025-01-08 18:15:26 +0900

    FE layout 수정

--- src/main/frontend/src/components/layout/ContentsArea.jsx
+++ src/main/frontend/src/components/layout/ContentsArea.jsx
@@ -5,10 +5,8 @@ const ContentsArea = styled.div`
     max-width: 2000px; /* 최대 너비 설정 */
     height: calc(100vh - 200px); /* 상하 여백 */
     flex-shrink: 0;
-    opacity: 0.7;
-    background: #483165;
+    background: rgba(72, 49, 101, 0.7);    
     border-radius: 10px;
-
 `;
 
 export default ContentsArea;
--- src/main/frontend/src/components/layout/HeaderLayout.jsx
+++ src/main/frontend/src/components/layout/HeaderLayout.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import Header from "./Header";
+import {Outlet} from "react-router-dom";
+import styled from "styled-components";
+
 
 const HeaderLayout = () => {
     return (
-        <>
-            <Header />
-        </>
+        <LayoutContainer>
+                <Header />
+            <ContentWrapper>
+                <Outlet />
+            </ContentWrapper>
+        </LayoutContainer>
     );
 };
 
 export default HeaderLayout;
+
+const LayoutContainer = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh; 
+`;
+
+const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    overflow-y: auto; // 콘텐츠가 크면 스크롤 가능
+`;
 
