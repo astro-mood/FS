d7d885550e9cdf6ed5083684f00ed23cfc722fa1
zzoeun <pje308@naver.com>
2025-01-10 01:40:26 +0900

    FE font 적용 / layout, router 수정

--- src/main/frontend/src/components/layout/GlobalStyle.jsx
+++ src/main/frontend/src/components/layout/GlobalStyle.jsx
@@ -2,6 +2,13 @@ import { createGlobalStyle } from 'styled-components';
 
 // 전역 스타일 관리 컴퍼넌트
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'PyeongChangPeace-Bold';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2206-02@1.0/PyeongChangPeace-Bold.woff2') format('woff2');
+        font-weight: 700;
+        font-style: normal;
+    }
+    
     body {
         margin: 0;
         padding: 0;
