35cea668f96c5a45b0ac5045de216b3792c46873
zzoeun <pje308@naver.com>
2025-01-10 01:36:16 +0900

    FE api 모듈화를 위한 세팅

---
+++ src/main/frontend/src/api/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import instance from './instance';
+
+// 고민글 작성 API WriteWorry-postWorry
+export const postWorry = async (data) => {
+    try {
+        const response = await instance.post("/worry", data);
+        return response.data; // 성공적으로 전송된 데이터 반환
+    } catch (error) {
+        console.error("API 요청 에러:", error);
+        throw error; // 에러를 호출한 곳에서 처리하도록 던짐
+    }
+};
+
