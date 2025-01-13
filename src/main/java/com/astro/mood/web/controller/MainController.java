074f51cd9f1d056cb5d5e160a07eecc225994ddd
opllip <opllip@naver.com>
2025-01-14 00:43:45 +0900

    #2 jwt관련 수정, 리프레시 토큰부분 수정

--- src/main/java/com/astro/mood/web/controller/MainController.java
+++ src/main/java/com/astro/mood/web/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.astro.mood.web.controller;
 
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,8 +11,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class MainController {
     // 루트 경로 요청 처리
     @GetMapping({"/", "/error"})
-    public String oauthLogin() {
-
+    public String indexPage(HttpServletRequest request, HttpServletResponse response) {
+        Cookie refreshTokenCookie = new Cookie("refreshToken", null);  // 쿠키 값을 null로 설정
+        refreshTokenCookie.setMaxAge(0);  // 남은 만료시간을 0으로 설정
+        response.addCookie(refreshTokenCookie);
         return "index.html";// index.html 파일을 반환
     }
+
 }
