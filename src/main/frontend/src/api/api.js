975650298aed84f4a72cb9591a1f8c8a35b2ff88
zzoeun <pje308@naver.com>
2025-01-14 01:29:44 +0900

    FE 일기작성 api 연동 로직 추가

--- src/main/frontend/src/api/api.js
+++ src/main/frontend/src/api/api.js
@@ -12,3 +12,15 @@ export const postWorry = async (data) => {
     }
 };
 
+
+// 일기작성 API WriteDiary-postDiary
+export const postDiary = async (data) => {
+    try {
+        const response = await instance.post("/writediary", data);
+        return response.data; // 성공적으로 전송된 데이터 반환
+    } catch (error) {
+        console.error("API 요청 에러:", error);
+        throw error; // 에러를 호출한 곳에서 처리하도록 던짐
+    }
+};
+
