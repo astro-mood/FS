398ce35062f4cfa4a0d1d7ccafa76e8a9c01d967
opllip <opllip@naver.com>
2025-01-14 22:23:04 +0900

    안쓰이는 import 삭제, 중복되는 코드 수정

--- src/main/java/com/astro/mood/data/repository/auth/AuthRepository.java
+++ src/main/java/com/astro/mood/data/repository/auth/AuthRepository.java
@@ -2,8 +2,6 @@ package com.astro.mood.data.repository.auth;
 
 import com.astro.mood.data.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
