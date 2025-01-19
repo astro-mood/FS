273207b54528961d303846cca25d3cc4e802feac
opllip <opllip@naver.com>
2025-01-20 00:37:44 +0900

    고민 댓글 CRUD, 신고 기능 추가

--- src/main/java/com/astro/mood/data/repository/worry/WorryCommentRepository.java
+++ src/main/java/com/astro/mood/data/repository/worry/WorryCommentRepository.java
@@ -2,6 +2,8 @@ package com.astro.mood.data.repository.worry;
 
 import com.astro.mood.data.entity.worry.WorryComment;
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +16,11 @@ public interface WorryCommentRepository  extends JpaRepository<WorryComment,Inte
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT wc FROM WorryComment wc WHERE wc.commentIdx = :commentIdx")
     Optional<WorryComment> findByCommentIdxWithLock(@Param("commentIdx") Integer commentIdx);
+
+    Page<WorryComment> findByWorryIdxAndParentCommentIsNull(Integer worryIdx, Pageable pageable);
+
+    Optional<WorryComment> findByCommentIdx( Integer commentIdx);
+
+    int countByParentCommentAndIsDeletedFalseAndIsReportedFalse(WorryComment parentComment);
+    int countByParentComment(WorryComment parentComment);
 }
